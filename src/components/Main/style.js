67a93743f096c1a37a9e51503240f059c74c95d4
Hiago Da Silva <hiagodasilvaconceicao@gmail.com>
2021-12-15 17:46:09 -0300

    projeto finalizado

--- src/components/Main/style.js
+++ src/components/Main/style.js
@@ -1,109 +1,110 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export  const Container = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    padding-top: 3rem;
-`
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding-top: 3rem;
+`;
 
 export const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 40vh;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 40vh;
+`;
 
 export const Title = styled.h1`
-    width: 360px;
-    color: rgb(34, 37, 75);
-`
+  width: 360px;
+  color: rgb(34, 37, 75);
+`;
 
 export const Paragraph = styled.p`
-    width: 380px;
-    color: rgb(34, 37, 75);
-    opacity: 0.9;
-`
+  width: 380px;
+  color: rgb(34, 37, 75);
+  opacity: 0.9;
+`;
 
 export const BtnStart = styled.button`
-    background: gold;
-    width: 150px;
-    border: 0;
-    cursor: pointer;
-    font-weight: bold;
-`
+  background: gold;
+  width: 200px;
+  border: 0;
+  cursor: pointer;
+  font-weight: bold;
+  height: 45px;
+`;
 
 export const BottomTitle = styled.h2`
-    color: rgb(34, 37, 75);
-`
+  color: rgb(34, 37, 75);
+`;
 
 export const BottomParagraph = styled.p`
-    width: 465px;
-    color: rgb(34, 37, 75);
-    opacity: 0.9;
-    text-align: center;
-`
+  width: 465px;
+  color: rgb(34, 37, 75);
+  opacity: 0.9;
+  text-align: center;
+`;
 
 export const BottomWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const BoxGif = styled.div`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    background-color: rgb(34, 37, 75);
-    height: 40vh;
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: rgb(34, 37, 75);
+  height: 40vh;
+`;
 
 export const ImageMain = styled.img`
-    width: 150px;
-`
+  width: 200px;
+`;
 
 export const TitleGif = styled.h2`
-    color: #fff;
-`
+  color: #fff;
+`;
 
 export const ParagraphGif = styled.p`
-    width: 450px;
-    color: #fff;
-`
+  width: 450px;
+  color: #fff;
+`;
 
 export const BoxImgMain = styled.div`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
 export const TitleImgMain = styled.h2`
-    width: 300px;
-    color: rgb(34, 37, 75);
-`
+  width: 300px;
+  color: rgb(34, 37, 75);
+`;
 
 export const ParagraphImgMain = styled.p`
-    color: rgb(34, 37, 75);
-`
+  color: rgb(34, 37, 75);
+`;
 
 export const BoxMain = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    background-color: rgb(34, 37, 75);
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  background-color: rgb(34, 37, 75);
+`;
 
 export const ParagraphMain = styled.p`
-    width: 700px;
-    color: #fff;
-`
+  width: 700px;
+  color: #fff;
+`;
 export const TitleMain = styled.h2`
-    width: 350px;
-    color: #fff;
-`
+  width: 350px;
+  color: #fff;
+`;
 
 export const WrapperMain = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
